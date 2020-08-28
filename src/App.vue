cdb96a0bdac87ceddb772e055fe0239ed222d977
Iurii-Hub <st.play31@gmail.com>
2020-08-28 17:43:55 +0300

    HW1add

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <!-- Homework Створити простий todolist В папці assests є скрін як приблизно має виглядати 
-  ає бути масив тудушок, в інпуті вписуємо туду і пушимо в масив за допомогою кнопки Add. 
+  має бути масив тудушок, в інпуті вписуємо туду і пушимо в масив за допомогою кнопки Add. 
   Видаляємо тудушки за допомогою кнопки delete. Якщо тудушок більше 10 кнопка повинна сховатись. -->
 
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{message}}</h1>
     
+    <!-- <button v-on:click="increase">Increase</button>
+    <button v-on:click="decrease">Decrease</button> -->
+
+    <button v-on:click="counter++">Increase</button>
+    <button v-on:click="counter--">Decrease</button>
+
+    <p>{{counter}}</p>
+    <p>{{counter > 10 ? "Greater than 10": "Smaller than 10" }}</p>
+    <p>result: {{result()}}</p>
 
 
 
@@ -13,6 +23,41 @@
 </template>
 
 <script>
+
+export default {
+  name: "App",
+
+  data () {
+    return {
+      message: "HomeWork-1",
+      counter: 0,
+      // result: "",
+    }
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+
+    result () {
+      return this.counter > 10 ? "Greater than 10": "Smaller than 10";
+    },
+
+    // increase () {
+    //   this.counter ++;
+    //   this.result = this.counter > 10 ? "Greater than 10": "Smaller than 10";
+    // },
+
+    // decrease () {
+    //   this.counter --;
+    //   this.result = this.counter > 10 ? "Greater than 10": "Smaller than 10";
+    // }
+
+  }
+}
+
 
 
 </script>
