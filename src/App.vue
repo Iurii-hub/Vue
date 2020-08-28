b93eba7e34714158445571d831e0ec3e2bb9d142
Iurii-Hub <st.play31@gmail.com>
2020-08-28 13:41:46 +0300

    Vue-HW1

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <!-- Homework Створити простий todolist В папці assests є скрін як приблизно має виглядати 
+  ає бути масив тудушок, в інпуті вписуємо туду і пушимо в масив за допомогою кнопки Add. 
+  Видаляємо тудушки за допомогою кнопки delete. Якщо тудушок більше 10 кнопка повинна сховатись. -->
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
